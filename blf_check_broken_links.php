eb12ad72e11c53810c040976ecbab1f5ea7893b4
Javier Trujillo <javier.trujillo.jimenez@gmail.com>
2024-08-16 19:48:16 +0200

    Añadimos la posibilidad de actualizar desde github

--- blf_check_broken_links.php
+++ blf_check_broken_links.php
@@ -4,6 +4,7 @@ Plugin Name: Broken Link Fixer
 Description: Busca y elimina enlaces rotos dentro de los posts .
 Version: 1.1
 Author: Misterdigital
+GitHub Plugin URI:https://github.com/eltictacdicta/blf_check_broken_links
 */
 
 // Evitar acceso directo
